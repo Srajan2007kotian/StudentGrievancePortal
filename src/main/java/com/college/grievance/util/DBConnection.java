44a245e88d2e08e2e6e4ecb96cefe13cc2ec8eef
Srajan Kotian <ce25.srajan.kotian@kccemsr.edu.in>
2026-09-23 21:03:06 +0530

    Connect portal to Render PostgreSQL

--- src/main/java/com/college/grievance/util/DBConnection.java
+++ src/main/java/com/college/grievance/util/DBConnection.java
@@ -1,25 +1,33 @@
 package com.college.grievance.util;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
 
-    private static final String URL =
-            "jdbc:mysql://localhost:3306/grievance_portal?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
-
-    private static final String USER = "grievance_user";
-
-    private static final String PASSWORD = "grievance123";
-
-    static {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-    }
-
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+
+        String databaseUrl = System.getenv("DATABASE_URL");
+
+        if (databaseUrl != null && !databaseUrl.isBlank()) {
+
+            // Render PostgreSQL URL
+            if (databaseUrl.startsWith("postgresql://")) {
+                databaseUrl = "jdbc:" + databaseUrl;
+            }
+
+            return DriverManager.getConnection(databaseUrl);
+        }
+
+        // Local MySQL connection
+        String url =
+                "jdbc:mysql://localhost:3306/grievance_portal" +
+                "?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+
+        String user = "grievance_user";
+        String password = "grievance123";
+
+        return DriverManager.getConnection(url, user, password);
     }
 }
